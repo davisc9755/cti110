0e12b3ba6e50fd8f1e1fc667b0c77cd38580c762
davisc9755 <145596245+davisc9755@users.noreply.github.com>
2023-11-06 14:42:38 -0500

    Add files via upload

---
+++ P4LAB1a_DavisChad.py
@@ -0,0 +1,24 @@
+import turtle
+
+mike = turtle.Turtle()
+
+mike.shape("turtle")
+
+for x in range (4):
+
+    mike.forward(50)
+    mike.left(90)
+    
+mike.penup()
+mike.forward(100)
+mike.pendown()
+
+for x in range (3):
+
+    mike.forward(60)
+    mike.left(120)
+    
+    
+    
+    
+
---
+++ P4LAB1b_DavisChad.py
@@ -0,0 +1,27 @@
+import turtle
+
+cliff = turtle.Turtle()
+
+for x in range (1):
+
+    cliff.right(180)
+    cliff.circle(50,180)
+
+cliff.penup()
+cliff.forward(50)
+cliff.pendown()
+
+for x in range (1):
+
+    cliff.circle(50,180)
+    cliff.left(90)
+    cliff.forward(100)
+
+cliff.penup()
+cliff.left(90)
+cliff.forward(50)
+cliff.pendown()
+    
+    
+
+    
